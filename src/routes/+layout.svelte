cc4e5f7491a629f9bfa18f7a4a7fe7b6f23d6544
Phil <dev@phunky.tech>
2024-05-22 11:56:30 -0600

    Draft done. Purple!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:root {
+		@apply w-screen min-h-screen;
+		@apply bg-gradient-to-br from-white to-purple-200;
+		@apply font-mono;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen md:h-screen flex flex-col my-8 sm:my-16 md:my-0 md:justify-center items-center">
+	<div class="flex flex-col text-md md:max-w-2xl sm:mx-16 mx-8">
+		<h1 class="text-3xl">Hey! I'm Phillip.</h1>
+		<p>I'm a freelance full-stack developer. I enjoy solving problems that make an impact.</p>
+		<br />
+		<h2 class="text-2xl">👨‍💻 Tech Stuff</h2>
+		<p>
+			I do web development by day, working primarily with Typescript, SvelteKit, and PostgreSQL.
+			Recently, I helped create <a
+				class="underline text-blue-600 hover:text-blue-400"
+				href="https://www.xynth.audio/">Xynth Audio</a
+			>'s website and admin tools.
+		</p>
+		<p>
+			I also build computers and dabble in homelabbing. Some of my favorite tools I host include <a
+				class="underline text-blue-600 hover:text-blue-400"
+				href="https://github.com/immich-app/immich">Immich</a
+			>,
+			<a
+				class="underline text-blue-600 hover:text-blue-400"
+				href="https://github.com/linkwarden/linkwarden">Linkwarden</a
+			>, and
+			<a
+				class="underline text-blue-600 hover:text-blue-400"
+				href="https://github.com/TomBursch/kitchenowl">KitchenOwl</a
+			>.
+		</p>
+		<br />
+		<h2 class="text-2xl">☀️ Non-Tech Stuff</h2>
+		<p>
+			When I'm not programming, you can find me reading, disc golfing, gaming, or making music. I
+			have also been known to play D&D when the stars align.
+		</p>
+		<br />
+		<h2 class="text-2xl">📬 Contact</h2>
+		<p>You can find me at any of the links below.</p>
+		<a class="my-2 flex flex-row hover:text-blue-600" href="mailto:contact@phillipbruce.com">
+			<img src="./envelope-solid.svg" alt="Email" class="size-10" />
+			<div class="ml-2 flex flex-col justify-center items-center">contact@phillipbruce.com</div>
+		</a>
+		<a
+			class="mb-2 flex flex-row hover:text-blue-600"
+			href="https://www.linkedin.com/in/phillip-b-4082b4254/"
+		>
+			<img src="./linkedin.svg" alt="LinkedIn" class="size-10" />
+			<div class="ml-2 flex flex-col justify-center items-center">Phillip Bruce</div>
+		</a>
+		<a class="my-2 flex flex-row hover:text-blue-600" href="https://github.com/phunky-tech">
+			<img src="./square-github.svg" alt="GitHub" class="size-10" />
+			<div class="ml-2 flex flex-col justify-center items-center">@phunky-tech</div>
+		</a>
+	</div>
+</div>
+
+<style lang="postcss">
+	p {
+		@apply my-2;
+	}
+
+	br {
+		@apply my-2;
+	}
+</style>
